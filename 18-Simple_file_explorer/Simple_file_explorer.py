827d9ca937dd2c7963978eaf69631cb6aa75fdf1
Emre Öztürk <ozturkemry@gmail.com>
2017-02-09 10:14:26 +0300

    Changed

--- 18-Simple_file_explorer/Simple_file_explorer.py
+++ 18-Simple_file_explorer/Simple_file_explorer.py
@@ -1,22 +1,16 @@
 #! usr/bin
 import os
-now="/home"
+now=os.getcwd()
 os.chdir(now)
 while True:
     print("\nCurrent directory : ")
-    os.system("pwd")
-    print("\nDirectories: ")
-    os.system("ls -d */")
-    print("\nFiles: ")
-    os.system("ls -p | grep -v /")
-    target=input("\nchange directory to : \n"
-              "2 to exit: \n")
-    if(target=="2"):
-        print("Goodbye")
-        exit()
-    else:
-        try:
-            now = now + "/" + target
-            os.chdir(now)
-        except:
-            print("Enter valid directory \n")
+    print(now)
+    print("\nList directory: ")
+    print(os.listdir(os.getcwd()))
+    target=input("\nchange directory to : \n")
+    try:
+        s.chdir(now)
+        now = now + "/" + target
+    except:
+        os.chdir(target)
+        now=target
