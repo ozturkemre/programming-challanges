03151b6e02171d6c022c8eb90fd6a13ce7625ac1
Emre Öztürk <ozturkemry@gmail.com>
2017-02-09 10:51:16 +0300

    Changed

--- 18-Simple_file_explorer/Simple_file_explorer.py
+++ 18-Simple_file_explorer/Simple_file_explorer.py
@@ -3,14 +3,26 @@ import os
 now=os.getcwd()
 os.chdir(now)
 while True:
-    print("\nCurrent directory : ")
-    print(now)
+    print("Working directory : ")
+    print(os.getcwd())
     print("\nList directory: ")
-    print(os.listdir(os.getcwd()))
+    space = 0
+    for i in os.listdir(os.curdir):
+        print("-"*space,">",i)
+        space += 1
     target=input("\nchange directory to : \n")
-    try:
-        s.chdir(now)
-        now = now + "/" + target
-    except:
-        os.chdir(target)
-        now=target
+
+    if os.path.exists(target):
+        if os.path.isdir(target):
+            os.chdir(target)
+            now=os.getcwd()
+        else:
+            temp = os.path.join(now,target)
+            if os.path.isfile(temp):
+                print("Can't change directory to file")
+            else:
+                if os.path.exists(temp):
+                    now = temp
+                    os.chdir(now)
+                else:
+                    print("No such file or directory\n")
