28c3c69dee8a82b6f4296daeb000da64dd0ab73f
Emre Öztürk <ozturkemry@gmail.com>
2017-02-10 22:43:43 +0300

    rewrite

--- 01-higher_lower/higher_lower.py
+++ 01-higher_lower/higher_lower.py
@@ -1,15 +1,45 @@
 import random
-number=random.randint(0,99)
-estimate=int(input("Guess the number: "))
-effort=0
-while True:
-    effort = effort + 1
-    if(estimate>number):
-        print("Lower")
-    elif(estimate<number):
-        print("Higher")
-    else:
-        break
-    estimate = int(input("Guess the number: "))
 
-print("Congratulations you found it. Number of trials:{}".format(effort))
+
+def create_number():
+    number = random.randint(0, 99)
+    return number
+
+
+def make_prediction():
+    return input("Guess the number: ")
+
+
+def check(guess, number):
+    if guess > number:
+        print("Lower")
+    elif guess < number:
+        print("Higher")
+    return guess == number
+
+
+def program():
+    number = create_number()
+    numberoftrials = 0
+    while numberoftrials <= 10:
+        guess = make_prediction()
+
+        try:
+            if check(int(guess), number):
+                print("Congratulations you found it. Number of trials: {}".format(numberoftrials))
+                break
+            else:
+                numberoftrials += 1
+
+        except ValueError:
+            print("Prediction must be number.")
+
+        except:
+            print("Something that should not give an error gave an error.")
+
+    if numberoftrials > 10:
+
+        print("Your predictions were wrong. Number was {}. Sorry :(".format(number))
+
+program()
+
